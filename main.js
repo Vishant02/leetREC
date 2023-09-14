e6315312d8fce51a105d72aaa842607c3a22ea77
Vishant02 <vishant.mittal233@gmail.com>
2023-09-14 17:22:30 +0530

    2824

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var countPairs = function(nums, target) {
+    let count=0
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]<target){
+                count=count+1;
+            }
+        }
+    }
+    return count
+};
+
+countPairs([-1,1,2,3,1],2)
